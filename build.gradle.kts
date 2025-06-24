7c8ef156e59ecc7f98943528ac96abd418a13e40
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2025-06-24 18:54:24 +0200

    build: move detekt and ktfmt versions to the version catalog for consistency

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    id("io.gitlab.arturbosch.detekt") version("1.23.8")
-    id("com.ncorti.ktfmt.gradle") version "0.23.0"
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.ktfmt)
 }
 
 tasks.register("lintKotlin") {
