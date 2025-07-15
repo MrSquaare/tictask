97da1d196408d5d9ae43ac99405b6bcf21af6b68
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2025-07-15 19:36:24 +0200

    test(android): improve activity test

--- androidApp/src/androidInstrumentedTest/kotlin/fr/mrsquaare/tictask/ActivityTest.kt
+++ androidApp/src/androidInstrumentedTest/kotlin/fr/mrsquaare/tictask/ActivityTest.kt
@@ -1,12 +1,11 @@
 package fr.mrsquaare.tictask
 
-import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.test.core.app.ActivityScenario
+import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
@@ -16,23 +15,25 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ActivityTest {
-    @get:Rule val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
     fun navigateToSettings_thenBackToHome_thenBackAgain_shouldCloseApp() {
-        ActivityScenario.launch(MainActivity::class.java).use { scenario ->
-            composeTestRule.onAllNodes(hasText("Home"))[0].assertIsDisplayed()
+        composeTestRule.onAllNodes(hasText("Home"))[0].assertIsDisplayed()
 
-            composeTestRule.onNodeWithText("Settings").performClick()
-            composeTestRule.onNodeWithText("Settings Screen").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Settings").performClick()
+        composeTestRule.onNodeWithText("Settings Screen").assertIsDisplayed()
 
-            composeTestRule.onNodeWithText("Home").performClick()
-            composeTestRule.onAllNodes(hasText("Home"))[0].assertIsDisplayed()
+        composeTestRule.onNodeWithText("Home").performClick()
+        composeTestRule.onAllNodes(hasText("Home"))[0].assertIsDisplayed()
 
-            val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-            device.pressBack()
+        val activity = composeTestRule.activity
 
-            composeTestRule.waitUntil(timeoutMillis = 5000) { scenario.state.name == "DESTROYED" }
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        device.pressBack()
+
+        composeTestRule.waitUntil(timeoutMillis = 5000) {
+            activity.lifecycle.currentState == Lifecycle.State.DESTROYED
         }
     }
 }
